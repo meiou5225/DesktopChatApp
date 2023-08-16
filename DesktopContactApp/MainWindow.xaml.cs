817bc810b0a502a18afbab60ca2c9d0774bfe68d
meiou5225 <josemayi@outlook.com>
2023-08-16 17:56:39 +0500

    Reading from the Table

--- DesktopContactApp/MainWindow.xaml.cs
+++ DesktopContactApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DesktopContactApp.Classes;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,12 +25,24 @@ namespace DesktopContactApp
 		public MainWindow()
 		{
 			InitializeComponent();
+			ReadDatabase();
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
 			NewContactWindow newContactWindow = new NewContactWindow();
 			newContactWindow.ShowDialog();
-        }
+			ReadDatabase();
+		}
+
+		void ReadDatabase() 
+		{
+			using (SQLiteConnection conn = new SQLiteConnection(App.databasePath))
+			{
+				conn.CreateTable<Contact>();
+				var contacts=conn.Table<Contact>().ToList();
+			}
+
+		}
     }
 }
