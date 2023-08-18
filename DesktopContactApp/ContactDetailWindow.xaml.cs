98321f8877ac65ee4820f716eafaa128418a12d0
meiou5225 <josemayi@outlook.com>
2023-08-18 22:21:09 +0500

    Update the SQLite Table

--- DesktopContactApp/ContactDetailWindow.xaml.cs
+++ DesktopContactApp/ContactDetailWindow.xaml.cs
@@ -27,13 +27,25 @@ namespace DesktopContactApp
         {
             InitializeComponent();
             this.contact = contact;
+			nameTextBox.Text = contact.Name;
+			phoneTextBox.Text = contact.Phone;
+			emailTextBox.Text = contact.Email;
           
         }
 
 		private void updateButton_Click(object sender, RoutedEventArgs e)
 		{
+			contact.Name = nameTextBox.Text;	
+			contact.Phone= phoneTextBox.Text;
+			contact.Email=emailTextBox.Text;
+			using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+			{
+				connection.CreateTable<Contact>();
+				connection.Update(contact);
+			}
+			Close();
 
-        }
+		}
 
 		private void deleteButton_Click(object sender, RoutedEventArgs e)
 		{
