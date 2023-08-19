152096e1dc42e7331825f399b961d5afd8134329
meiou5225 <josemayi@outlook.com>
2023-08-20 02:49:45 +0500

    Binding to a Custom Control - Dependency Properties

--- DesktopContactApp/Controls/ContactControl.xaml.cs
+++ DesktopContactApp/Controls/ContactControl.xaml.cs
@@ -21,20 +21,44 @@ namespace DesktopContactApp.Controls
     /// </summary>
     public partial class ContactControl : UserControl
     {
-        private Contact contact;
+        //      private Contact contact;
+
+        //      public Contact Contact
+        //{
+        //          get { return contact; }
+        //          set { 
+        //              contact = value;
+        //              nameTextBlock.Text = contact.Name;
+        //		emailTextBlock.Text = contact.Email;
+        //		phoneTextBlock.Text=contact.Phone;
+        //	}
+        //      }
+
+
 
         public Contact Contact
 		{
-            get { return contact; }
-            set { 
-                contact = value;
-                nameTextBlock.Text = contact.Name;
-				emailTextBlock.Text = contact.Email;
-				phoneTextBlock.Text=contact.Phone;
-			}
+            get { return (Contact)GetValue(ContactProperty); }
+            set { SetValue(ContactProperty, value); }
         }
 
-        public ContactControl()
+        // Using a DependencyProperty as the backing store for Contact.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactProperty =
+            DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactControl), new PropertyMetadata(new Contact() {Name="Name Lastname",Phone="(123)456 789",Email="example@domain.com" },SetText));
+
+		private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			ContactControl control=d as ContactControl;
+
+            if (control != null) 
+            {
+            control.nameTextBlock.Text = (e.NewValue as Contact).Name;
+            control.emailTextBlock.Text = (e.NewValue as Contact).Email;
+            control.phoneTextBlock.Text = (e.NewValue as Contact).Phone;
+            }
+		}
+
+		public ContactControl()
         {
             InitializeComponent();
         }
